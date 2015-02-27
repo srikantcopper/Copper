a8362b310b5bca485ab1b1db38d56d24dd478d8d
Aakash Sareen <aakash@coppermobile.com>
2015-02-27 15:29:56 -0800

    Initial Commit

---
+++ HonestBabyTests/HonestBabyTests.swift
@@ -0,0 +1,36 @@
+//
+//  HonestBabyTests.swift
+//  HonestBabyTests
+//
+//  Created by Aakash Sareen on 2/27/15.
+//  Copyright (c) 2015 Aakash Sareen. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class HonestBabyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
